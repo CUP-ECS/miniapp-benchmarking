a9811d7c45ab03e861600f22c5bf6d57ec3c4270
nicholas <nbacon@unm.edu>
2025-01-21 14:47:11 -0700

    Adding cli support

--- examples/main.cpp
+++ examples/main.cpp
@@ -23,47 +23,66 @@ int main(int argc, char** argv)
 {
 	try {
 
-		// Define the command line object, and insert a message
-		// that describes the program. The "Command description message"
-		// is printed last in the help text. The second argument is the
-		// delimiter (usually space) and the last one is the version number.
-		// The CmdLine object parses the argv array based on the Arg objects
-		// that it contains.
+
 		TCLAP::CmdLine cmd("Command description message", ' ', "0.9");
+		TCLAP::ValueArg<std::string> filepathArg("f","filepath","tspecify the path to the BENCHMARK_CONFIG file",true,"XXX","string");
+		TCLAP::ValueArg<int>         typesizeArg("t","typesize","the size of the variable being sent (in bytes)",true,0,"int");
+		TCLAP::ValueArg<int>         samplesArg("I","samples","the number of random samples to generate",true,0,"int");
+		TCLAP::ValueArg<int>         neighborsArg("n","neighbors","specify the average number of neighbors each process communicates with",true,0,"int");
+		TCLAP::ValueArg<int>         iterationsArg("i","iterations","specify the number of updates each sample performs",true,0,"int");
+		TCLAP::ValueArg<int>         neighbors_stdvArg("N","neighbors_stdv","specify the stdev number of neighbors each process communicates with",true,0,"int");
+		TCLAP::ValueArg<int>         owned_avgArg("o","owned_avg","x",true,0,"int");
+		TCLAP::ValueArg<int>         owned_stdvArg("O","owned_stdv","x",true,0,"int");
+		TCLAP::ValueArg<int>         remote_avgArg("r","remote_avg","x",true,0,"int");
+		TCLAP::ValueArg<int>         remote_stdArg("R","remote_std","x",true,0,"int");
+		TCLAP::ValueArg<int>         blocksize_avgArg("b","blocksize_avg","x",true,0,"int");
+		TCLAP::ValueArg<int>         blocksize_stdvArg("B","blocksize_stdv","x",true,0,"int");
+		TCLAP::ValueArg<int>         strideArg("s","stride","x",true,0,"int");
+		TCLAP::ValueArg<int>         seedArg("S","seed","x",true,0,"int");
+		TCLAP::ValueArg<int>         stride_stdvArg("T","stride_stdv","x",true,0,"int");
+		TCLAP::ValueArg<int>         memspaceArg("m","memspace","x",true,0,"int");
+		TCLAP::ValueArg<int>         distributionArg("d","distribution","x",true,0,"int");
+		TCLAP::ValueArg<int>         unitsArg("u","units","x",true,0,"int");
 
-		// Define a value argument and add it to the command line.
-		// A value arg defines a flag and a type of value that it expects,
-		// such as "-n Bishop".
-		TCLAP::ValueArg<std::string> nameArg("n","name","Name to print",true,"homer","string");
 
-		// Add the argument nameArg to the CmdLine object. The CmdLine object
-		// uses this Arg to parse the command line.
-		cmd.add( nameArg );
 
-		// Define a switch and add it to the command line.
-		// A switch arg is a boolean argument and only defines a flag that
-		// indicates true or false.  In this example the SwitchArg adds itself
-		// to the CmdLine object as part of the constructor.  This eliminates
-		// the need to call the cmd.add() method.  All args have support in
-		// their constructors to add themselves directly to the CmdLine object.
-		// It doesn't matter which idiom you choose, they accomplish the same thing.
-		TCLAP::SwitchArg reverseSwitch("r","reverse","Print name backwards", cmd, false);
+        TCLAP::SwitchArg reverseSwitch("t","typesize","the size of the variable being sent (in bytes)", cmd, false);
+
+
+
+
+        cmd.add(filepathArg)
+        cmd.add(typesizeArg)
+        cmd.add(samplesArg)
+        cmd.add(neighborsArg)
+        cmd.add(iterationsArg)
+        cmd.add(neighbors_stdvArg)
+        cmd.add(owned_avgArg)
+        cmd.add(owned_stdvArg)
+        cmd.add(remote_avgArg)
+        cmd.add(remote_stdArg)
+        cmd.add(blocksize_avgArg)
+        cmd.add(blocksize_stdvArg)
+        cmd.add(strideArg)
+        cmd.add(seedArg)
+        cmd.add(stride_stdvArg)
+        cmd.add(memspaceArg)
+        cmd.add(distributionArg)
+        cmd.add(unitsArg)
+
+
+
+
 
 		// Parse the argv array.
 		cmd.parse( argc, argv );
 
 		// Get the value parsed by each arg.
-		std::string name = nameArg.getValue();
-		bool reverseName = reverseSwitch.getValue();
+		std::string name = filepathArg.getValue();
+		std::cout << "My name is: " << name << std::endl;
 
-		// Do what you intend.
-		if ( reverseName )
-		{
-			std::reverse(name.begin(),name.end());
-			std::cout << "My name (spelled backwards) is: " << name << std::endl;
-		}
-		else
-			std::cout << "My name is: " << name << std::endl;
+
+
 
 
 	} catch (TCLAP::ArgException &e)  // catch exceptions
