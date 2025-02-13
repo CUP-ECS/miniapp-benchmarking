7fbe21146b9e749efca119fb080b6e62ba779c7f
nicholas <nbacon@unm.edu>
2025-02-12 23:20:42 -0700

    working on cabana

--- examples/main.cpp
+++ examples/main.cpp
@@ -96,316 +96,87 @@ int empirical_dist(const char* param) {
 //    // set the parameter string to look for.
     char param_key[25] = "PARAM: ";
     strcat(param_key, param);
-//
-//    // attempts to open the file pointer
-//    // and ensure that the file can be read
-//    // exits if fails
-//    FILE* fp = fopen(filepath, "r");
-//    if (fp == NULL) {
-//        fprintf(stderr, "Error: Unable to open file %s\n", filepath);
-//        exit(1);
-//    }
-//
-//    // stores the current read line and
-//    // some auxiliary information that assists in parsing the file
-//    char* line = NULL;
-//    size_t linecap = 0;
-//    ssize_t linelen;
-//
-//    // iterates through lines in the file until we get to the line
-//    // that contains the parameter we're trying to generate for
-//    while ((linelen = getline(&line, &linecap, fp)) != -1) {
-//        // remove trailing new line to better do comparison
-//        if (linelen > 0 && line[linelen-1] == '\n') {
-//            line[linelen-1] = '\0';
-//        }
-//
-//        // if the correct parameter is found,
-//        // stop iterating through the file
-//        if (strcmp(param_key, line) == 0) {
-//            break;
-//        }
-//    }
-//
-//    int binCount;
-//    int dataMin;
-//    int dataMax;
-//    int dataMean;
-//    int dataStdev;
-//
-//    // Iterates through non-bin data points
-//    // and collect information about the data and number of bins
-//    for (int i = 0; i < 5; i++) {
-//        // gets the next line which contains the BIN_COUNT data
-//        linelen = getline(&line, &linecap, fp);
-//
-//        // remove trailing new line to better do comparison
-//        if (linelen > 0 && line[linelen-1] == '\n') {
-//            line[linelen-1] = '\0';
-//        }
-//
-//        // gets the name of the data point being read
-//        char* token = strtok(line, ":");
-//
-//        // writes the correct token value to the correct variable from file input
-//        if (strcmp(token, "BIN_COUNT") == 0) {
-//            binCount = atoi(strtok(NULL, " "));
-//        } else if (strcmp(token, "MIN") == 0) {
-//            dataMin = atoi(strtok(NULL, " "));
-//        } else if (strcmp(token, "MAX") == 0) {
-//            dataMax = atoi(strtok(NULL, " "));
-//        } else if (strcmp(token, "MEAN") == 0) {
-//            dataMean = atoi(strtok(NULL, " "));
-//        } else if (strcmp(token, "STDEV") == 0) {
-//            dataStdev = atoi(strtok(NULL, " "));
-//        } else {
-//            printf("Error: reading data file failed, invalid data point found: %s\n", token);
-//            exit(1);
-//        }
-//    }
-//
-//    // do a quick sanity check to ensure that the data doesn't only
-//    // fall into a single bin
-//    if (dataMin == dataMax) {
-//        // close file as reading is no longer required
-//        fclose(fp);
-//
-//        return dataMin; // could also return data max, it is arbitrary
-//    }
-//
-//    // chose a uniformly distributed value between (0,1)
-//    double binSelection = (double)rand() / RAND_MAX;
-//
-//
-//    // used to track if we have checked a bin
-//    // if we have/are, the bin's proportion of the total dataset
-//    // is added to the binSum.
-//    // If the binSelection falls under the binSum, it is said
-//    // to be in the bin and we can use that bin's data to generate values
-//    double binSum = 0.0;
-//
-//    // iterates through the bins for a parameter to identity
-//    // which one should be chosen based on the random binSelection
-//    // value chosen above
-//    for (int i = 0; i < binCount; i++) {
-//        // gets the next line which contains the BIN_COUNT data
-//        linelen = getline(&line, &linecap, fp);
-//
-//        // remove trailing new line to better do comparison
-//        if (linelen > 0 && line[linelen-1] == '\n') {
-//            line[linelen-1] = '\0';
-//        }
-//
-//        // stores data from the current bin being checked
-//        int binMin;
-//        int binMax;
-//        int binMean;
-//        int binStdev;
-//
-//        // gets the bin minimum data point
-//        char* token = strtok(line, ",");
-//        binMin = atoi(token);
-//
-//        // gets the bin maximum data point
-//        token = strtok(NULL, ",");
-//        binMax = atoi(token);
-//
-//        // gets the binProp value and adds it to the binSum
-//        // NOTE: I don't think this will ever chose a bin
-//        //       that has a prop value of 0, as binSum would remain
-//        //       the same and would not trigger the binSelection check
-//        token = strtok(NULL, ",");
-//        binSum += strtod(token, NULL);
-//
-//        // if so, then the selection is in the most
-//        // recently searched bin, so we can proceed
-//        // to generating a random value
-//        // also triggers if on the last bin and no value has been selected
-//        if (binSelection < binSum || i == (binCount-1)) {
-//
-//            // if the bin is the correct one,
-//            // get the mean of the data points
-//            // for this bin
-//            token = strtok(NULL, ",");
-//            binMean = atoi(token);
-//
-//            // if the bin is the correct one,
-//            // get the standard deviation of the
-//            // data points in this bin
-//            token = strtok(NULL, ",");
-//            binStdev = atoi(token);
-//
-//            // close file as reading is no longer required
-//            fclose(fp);
-//
-//            // return a normally distributed value with the
-//            // mean and standard deviation from the
-//            // chosen bin
-//            return gauss_dist(binMean, binStdev);
-//        }
-//
-//    }
-//
-//    // if we have somehow made it to this point
-//    // without generating a value, we should throw an error
-//    printf("Error: empirical value could not be generated due to an unknown error.\n");
-//    printf("Parameter type being generated: %s\n", param);
-//    printf("Config file which could have caused this error: %s\n", filepath);
-//    printf("If you encounter a bug which causes this for any reason, please open an issue on Github\n");
-//    printf("Here is additional debugging information\n");
-//    printf("\tbinSelection: %f\n", binSelection);
-//    printf("\tbinSum: %f\n", binSum);
-//    exit(1);
+
     return -1;
 }
 
 
-int benchmark(int penum) {
-    // for benchmarks with irregularity disabled,
-    // having more than 1 sample is not useless
-    if (irregularity == 0) {
-        if (penum == 0) {
-            printf("Irregularity has been disabled, setting nsamples to 1...\n");
-            printf("To increase iterations for benchmarks with irregularity disabled, please specify parameters via the \"-i [iterations]\"\n");
-        }
-        nsamples = 1;
-    }
-
-    // stores original parameter values if set by the user
-    // this is because the original values change with irregularity enabled
-    // and the new starting points for each iteration need to be set
-    int nowned_orig = nowned;
-    int nneighbors_orig = nneighbors;
-    int nremote_orig = nremote;
-    int blocksz_orig = blocksz;
-    int stride_orig = stride;
-
-    for(int sample_iter = 0; sample_iter < nsamples+1; sample_iter++) {
-        // if irregularity is enabled, perform a reference benchmark first
-        if (irregularity) {
-            if (sample_iter == 0) {
-                // if this is the first iteration, print the reference benchmark information
-                if (penum == 0) {
-                    printf("Non-Irregular Reference Benchmark\n");
-                }
-            } else {
-                /*
-                // if irregularity is enabled, set random parameters
-                // the following code determines if the default values should
-                // be used as random generation starting points
-                // if a value is set by the user, it makes sense to use that
-                // parameter as a starting point for all iterations values
-                // if a value is unset, the benchmark's default values are used
-                // as the starting point (mean, stdev) for the gaussian dist
-                */
-                if (irregularity_owned) {
-                    switch (distribution_type) {
-                        case GAUSSIAN:
-                            nowned = gauss_dist(nowned_orig, nowned_stdv);
-                            break;
-                        case EMPIRICAL:
-                            nowned = empirical_dist("nowned");
-                            break;
-                        default:
-                            nowned = nowned_orig;
-                            break;
-                    }
-                }
-                if (irregularity_remote) {
-                    switch (distribution_type) {
-                        case GAUSSIAN:
-                            nremote = gauss_dist(nremote_orig, nremote_orig);
-                            break;
-                        case EMPIRICAL:
-                            nremote = empirical_dist("nremote");
-                            break;
-                        default:
-                            nremote = nremote_orig;
-                            break;
-                    }
-                }
-                if (irregularity_blocksz) {
-                    switch (distribution_type) {
-                        case GAUSSIAN:
-                            blocksz = gauss_dist(blocksz_orig, blocksz_stdv);
-                            break;
-                        case EMPIRICAL:
-                            blocksz = empirical_dist("blocksize");
-                            break;
-                        default:
-                            blocksz = blocksz_orig;
-                            break;
-                    }
-                }
-                if (irregularity_neighbors) {
-                    switch (distribution_type) {
-                        case GAUSSIAN:
-                            nneighbors = gauss_dist(nneighbors_orig, nneighbors_stdv);
-                            break;
-                        case EMPIRICAL:
-                            nneighbors = empirical_dist("comm_partners");
-                            break;
-                        default:
-                            nneighbors = nneighbors_orig;
-                            break;
-                    }
-                }
-                if (irregularity_stride) {
-                    switch (distribution_type) {
-                        case GAUSSIAN:
-                            stride = gauss_dist(stride_orig, stride_stdv);
-                            break;
-                        case EMPIRICAL:
-                            stride = empirical_dist("stride");
-                            break;
-                        default:
-                            stride = stride_orig;
-                            break;
-                    }
-                }
-
-                if (report_params) {
-                    printf("PARAM: nowned - %d\n", nowned);
-                    printf("PARAM: nremote - %d\n", nremote);
-                    printf("PARAM: blocksize - %d\n", blocksz);
-                    printf("PARAM: stride - %d\n", stride);
-                    printf("PARAM: nneighbors - %d\n", nneighbors);
-                }
-
-                // print benchmark status each iteration
-                if (penum == 0) {
-                    printf("Benchmark Iteration: %d/%d\n", sample_iter, nsamples);
-                }
-            }
-        } else {
-            if (sample_iter == 0) {
-                nsamples--;
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-    return 0;
-}
-
 void migrationExample()
 {
-    std::cout << "Cabana Migration Example\n" << std::endl;
 
-    /*
-       Get parameters from the communicator. We will use MPI_COMM_WORLD for
-       this example but any MPI communicator may be used.
-    */
+  int nowned_orig = nowned;
+  int nneighbors_orig = nneighbors;
+  int nremote_orig = nremote;
+  int blocksz_orig = blocksz;
+  int stride_orig = stride;
+
+  for(int sample_iter = 0; sample_iter < nsamples+1; sample_iter++) {
+
+    nowned = gauss_dist(nowned_orig, nowned_stdv);
+    nremote = gauss_dist(nremote_orig, nremote_orig);
+    blocksz = gauss_dist(blocksz_orig, blocksz_stdv);
+    nneighbors = gauss_dist(nneighbors_orig, nneighbors_stdv);
+    stride = gauss_dist(stride_orig, stride_stdv);
+
+    if (report_params) {
+      printf("PARAM: nowned - %d\n", nowned);
+      printf("PARAM: nremote - %d\n", nremote);
+      printf("PARAM: blocksize - %d\n", blocksz);
+      printf("PARAM: stride - %d\n", stride);
+      printf("PARAM: nneighbors - %d\n", nneighbors);
+    }
+
+
+
+
+
     int comm_rank = -1;
     MPI_Comm_rank( MPI_COMM_WORLD, &comm_rank );
     int comm_size = -1;
     MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
+
+
+    int remainder = nneighbors % 2;
+    int num_partners_lo, num_partners_hi;
+    int offset;
+    std::vector<int> partner_pe(nneighbors);
+
+
+    // Adjust the number of partners based on comm_rank
+    if (comm_rank < (nneighbors / 2)) {
+      num_partners_lo = nneighbors / 2;
+      num_partners_hi = nneighbors / 2 + remainder;
+    } else {
+      num_partners_lo = nneighbors / 2 + remainder;
+      num_partners_hi = nneighbors / 2;
+    }
+
+    // Partners below the current rank
+    offset = 0;
+
+    for (int i=1; i <= num_partners_lo; i++) {
+      int partner = (i > comm_rank) ? (comm_size+ comm_rank - i) : comm_size - 1;
+      //printf("[pe %d] offset %d penum %d i %d \n",penum, offset, penum, i);
+      partner_pe[offset] = partner;
+      offset++;
+    }
+
+    /* Indices above this PE */
+    for (int i=1; i<=num_partners_hi; i++){
+      int partner = (i + comm_rank >= comm_size) ? i + comm_rank - comm_size : i + comm_rank;
+      //printf("[pe %d] offset %d penum %d i %d \n",penum, offset, penum, i);
+      partner_pe[offset] = partner;
+      offset++;
+    }
+
+
+
+
+    // Output the partners
+    for (int i = 0; i < partner_pe.size(); i++) {
+      std::cout << "Partner " << i << ": " << partner_pe[i] << std::endl;
+    }
+
 
     /*
       Declare the AoSoA parameters.
@@ -425,6 +196,9 @@ void migrationExample()
         slice_ranks( i ) = comm_rank;
         slice_ids( i ) = i+(100*comm_rank);
     }
+
+
+
 
 //    if ( comm_rank == 0 )
     {
@@ -460,6 +234,12 @@ void migrationExample()
     // The last 80 elements stay on this process.
     for ( int i = 20; i < num_tuple; ++i )
         export_ranks( i ) = comm_rank;
+
+
+
+
+
+
 
     std::vector<int> neighbors = { previous_rank, comm_rank, next_rank };
     std::sort( neighbors.begin(), neighbors.end() );
@@ -501,6 +281,7 @@ void migrationExample()
         std::cout << std::endl
                   << "(" << slice_ids.size() << " IDs after migrate)"
                   << std::endl;
+    }
     }
 }
 
